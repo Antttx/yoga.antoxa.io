d37c63d06f6ee71bda86e5b7cdf1ea04da05e5e3
Anton <ant.oxa@bk.ru>
2019-12-03 12:12:00 +0500

    add Calculator

--- js/script.js
+++ js/script.js
@@ -243,4 +243,54 @@ window.addEventListener('DOMContentLoaded', function () {
 	/* конец Slider */
 
 	/* Calculator */
+	let persons = document.querySelectorAll('.counter-block-input')[0],
+		restDays = document.querySelectorAll('.counter-block-input')[1],
+		place = document.getElementById('select'),
+		totalValue = document.getElementById('total'),
+		personsSum = 0,
+		daysSum = 0,
+		total = 0;
+
+	totalValue.innerHTML = 0;
+
+	persons.addEventListener('input', function(){
+
+		personsSum = +this.value;
+		total = (personsSum + daysSum) * 4000 * place.options[place.selectedIndex].value;
+
+		if(this.value == "" || restDays.value == ""){
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = total;
+		}
+	
+	});
+
+	restDays.addEventListener('input', function(){
+		
+		daysSum = +this.value;
+		total = (personsSum + daysSum) * 4000 * place.options[place.selectedIndex].value;
+
+		if(this.value == "" || persons.value == ""){
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = total;
+		}
+	
+	});
+
+	place.addEventListener('change', function(){
+
+		if(persons.value == "" || restDays == ""){
+			totalValue.innerHTML = 0;
+		} else {
+			let a = total;
+			totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+		}
+
+	});
+
+	/* конец Calculator */
+
+	
 });
